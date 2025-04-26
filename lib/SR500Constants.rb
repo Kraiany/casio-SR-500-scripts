2256f2b6fdca189f393e7648af3a189f6f5e5491
Dmytro Kovalov <dmytro@smartpay.co>
2025-04-26 22:47:17 +0900

    Add comments/translations for terms

--- lib/SR500Constants.rb
+++ lib/SR500Constants.rb
@@ -16,10 +16,10 @@ module SR500Constants
     items:          "PLU",
     taxableamount:  "TaxableAmount /対象計",
     tax_percent:    "Tax  %",
-    tax_amount:     "Total Amount",
+    tax_amount:     "Total Amount /合計",      # (ごうけい - gōkei)
     taxincluded:    "Tax Included  /内税",
-    amountreceived: "Amount Received /お預り",
-    amountreturned: "Amount Returned /お釣",
-    cash:           "Cash /現金",
+    amountreceived: "Amount Received /お預り", # お預り (おあずかり - oazukari)
+    amountreturned: "Amount Returned /お釣",   # お釣 (おつり - otsuri) -- End of receipt
+    cash:           "Cash /現金",              # 現金 (げんきん - genkin) -- End of receipt
   }
 end
